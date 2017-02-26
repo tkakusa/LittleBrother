622c58a4444a841aa01fc97d648b2f62b5cb1bf5
Takondwa Kakusa <tk421@vt.edu>
2017-02-26 07:01:25 +0000

    changed client code

--- host.py
+++ host.py
@@ -9,16 +9,82 @@ else:
     host_number = input("Which host is this? ")
 
 # establish a connection with the rabbitmq broker
-connection = rmq.rmq_connection('172.29.42.45', 'little_brother', 'host'+host_number, 'host'+host_number+'password')
+connection = rmq.rmq_connection('172.25.18.104', 'little_brother', 'host'+host_number, 'host'+host_number+'password')
 
 
 # do things and send messages about it indefinitely
 i = 0
 while(1):
-    message = {"host": host_number, "message count": i}
+    message = {
+    "host1": {
+        "cpu": {
+            "hi": 0,
+            "lo": 0 },
+        "lo": {
+            "rx": {
+                "hi": 0,
+                "lo": 0 },
+            "tx": {
+                "hi": 0,
+                "lo": 0 },
+            },
+        "eth0": {
+            "rx": {
+                "hi": 0,
+                "lo": 0 },
+            "tx": {
+                "hi": 0,
+                "lo": 0 },
+            },
+        "wlan0": {
+            "rx": {
+                "hi": 0,
+                "lo": 0 },
+            "tx": {
+                "hi": 0,
+                "lo": 0 },
+            },
+        },
+    "host2": {
+        "cpu": {
+            "hi": 0,
+            "lo": 0 },
+        "lo": {
+            "rx": {
+                "hi": 0,
+                "lo": 0 },
+            "tx": {
+                "hi": 0,
+                "lo": 0 },
+            },
+        "eth0": {
+            "rx": {
+                "hi": 0,
+                "lo": 0 },
+            "tx": {
+                "hi": 0,
+                "lo": 0 },
+            },
+        "wlan0": {
+            "rx": {
+                "hi": 0,
+                "lo": 0 },
+            "tx": {
+                "hi": 0,
+                "lo": 0 },
+            },
+        }
+    }
     rmq.rmq_publish(connection, json.dumps(message), 'host'+host_number)
     i = i + 1
     sleep(2)
+'''
+message = 
+
+rmq.rmq_publish(connection, json.dumps(message), 'host'+host_number)
+sleep(2)
+
+'''
 
 # close the connection once we're done
 connection.close()
--- mongoclient.py
+++ mongoclient.py
@@ -83,13 +83,13 @@ metrics = {
     }
     
 try:
-    opts, args = getopts.getopts(argv, "hb:p:c:k:")
+    opts, args = getopt.getopt(sys.argv[1:], "hb:p:c:k:")
 except getopt.GetoptError:
-    print 'pistatsview -b message broker [-p virtual host] [-c login:password] -k routing key'
+    print ('pistatsview -b message broker [-p virtual host] [-c login:password] -k routing key')
     sys.exit(2)
 for opt, arg in opts:
     if opt == '-h':
-        print 'pistatsview -b message broker [-p virtual host] [-c login:password] -k routing key'
+        print ('pistatsview -b message broker [-p virtual host] [-c login:password] -k routing key')
         sys.exit()
     elif opt in ("-b"):
         address = arg
@@ -99,31 +99,38 @@ for opt, arg in opts:
         (usr, pswd) = arg.split(":")
     elif opt in ("-k"):
         routing_key = arg
+
+print ("Address:        ", address)
+print ("Virtual Host:   ", vhost)
+print ("User Name:      ", usr)
+print ("Password:       ", pswd)
+print ("Routing Key:    ", routing_key)
         
 
 # When you receive messages, this function will be called. It needs to have these
 # arguments to be called properly. The routing key and message itself can be extracted
 # as shown in this example method.
 def callback(ch, method, properties, body):
-    print(" [x] Rxd from: " + method.routing_key + " Message: " + body.decode("utf-8"))
-    metrics[method.routing_key]['cpu']['hi'] = body[method.routing_key]['cpu']['hi'] if body[method.routing_key]['cpu']['hi'] > metrics[method.routing_key]['cpu']['hi']
-    metrics[method.routing_key]['cpu']['lo'] = body[method.routing_key]['cpu']['lo'] if body[method.routing_key]['cpu']['lo'] < metrics[method.routing_key]['cpu']['lo']
-    metrics[method.routing_key]['lo']['rx']['hi'] = body[method.routing_key]['lo']['rx']['hi'] if body[method.routing_key]['lo']['rx']['hi'] > metrics[method.routing_key]['lo']['rx']['hi']
-    metrics[method.routing_key]['lo']['rx']['lo'] = body[method.routing_key]['lo']['rx']['lo'] if body[method.routing_key]['lo']['rx']['lo'] < metrics[method.routing_key]['lo']['rx']['lo']
-    metrics[method.routing_key]['lo']['tx']['hi'] = body[method.routing_key]['lo']['tx']['hi'] if body[method.routing_key]['lo']['tx']['hi'] > metrics[method.routing_key]['lo']['tx']['hi']
-    metrics[method.routing_key]['lo']['tx']['lo'] = body[method.routing_key]['lo']['tx']['lo'] if body[method.routing_key]['lo']['tx']['lo'] < metrics[method.routing_key]['lo']['tx']['lo']
-    metrics[method.routing_key]['eth0']['rx']['hi'] = body[method.routing_key]['eth0']['rx']['hi'] if body[method.routing_key]['eth0']['rx']['hi'] > metrics[method.routing_key]['eth0']['rx']['hi']
-    metrics[method.routing_key]['eth0']['rx']['lo'] = body[method.routing_key]['eth0']['rx']['lo'] if body[method.routing_key]['eth0']['rx']['lo'] < metrics[method.routing_key]['eth0']['rx']['lo']
-    metrics[method.routing_key]['eth0']['tx']['hi'] = body[method.routing_key]['eth0']['tx']['hi'] if body[method.routing_key]['eth0']['tx']['hi'] > metrics[method.routing_key]['eth0']['tx']['hi']
-    metrics[method.routing_key]['eth0']['tx']['lo'] = body[method.routing_key]['eth0']['tx']['lo'] if body[method.routing_key]['eth0']['tx']['lo'] < metrics[method.routing_key]['eth0']['tx']['lo']
-    metrics[method.routing_key]['wlan0']['rx']['hi'] = body[method.routing_key]['wlan0']['rx']['hi'] if body[method.routing_key]['wlan0']['rx']['hi'] > metrics[method.routing_key]['wlan0']['rx']['hi']
-    metrics[method.routing_key]['wlan0']['rx']['lo'] = body[method.routing_key]['wlan0']['rx']['lo'] if body[method.routing_key]['wlan0']['rx']['lo'] < metrics[method.routing_key]['wlan0']['rx']['lo']
-    metrics[method.routing_key]['wlan0']['tx']['hi'] = body[method.routing_key]['wlan0']['tx']['hi'] if body[method.routing_key]['wlan0']['tx']['hi'] > metrics[method.routing_key]['wlan0']['tx']['hi']
-    metrics[method.routing_key]['wlan0']['tx']['lo'] = body[method.routing_key]['wlan0']['tx']['lo'] if body[method.routing_key]['wlan0']['tx']['lo'] < metrics[method.routing_key]['wlan0']['tx']['lo']    
+    print ("Entered")
+    metrics[method.routing_key]['cpu']['hi'] = body[method.routing_key]['cpu']['hi'] if body[method.routing_key]['cpu']['hi'] > metrics[method.routing_key]['cpu']['hi'] else metrics[method.routing_key]['cpu']['hi']
+    metrics[method.routing_key]['cpu']['lo'] = body[method.routing_key]['cpu']['lo'] if body[method.routing_key]['cpu']['lo'] < metrics[method.routing_key]['cpu']['lo'] else metrics[method.routing_key]['cpu']['lo']
+    metrics[method.routing_key]['lo']['rx']['hi'] = body[method.routing_key]['lo']['rx']['hi'] if body[method.routing_key]['lo']['rx']['hi'] > metrics[method.routing_key]['lo']['rx']['hi'] else metrics[method.routing_key]['lo']['rx']['hi']
+    metrics[method.routing_key]['lo']['rx']['lo'] = body[method.routing_key]['lo']['rx']['lo'] if body[method.routing_key]['lo']['rx']['lo'] < metrics[method.routing_key]['lo']['rx']['lo'] else metrics[method.routing_key]['lo']['rx']['lo']
+    metrics[method.routing_key]['lo']['tx']['hi'] = body[method.routing_key]['lo']['tx']['hi'] if body[method.routing_key]['lo']['tx']['hi'] > metrics[method.routing_key]['lo']['tx']['hi'] else metrics[method.routing_key]['lo']['tx']['hi']
+    metrics[method.routing_key]['lo']['tx']['lo'] = body[method.routing_key]['lo']['tx']['lo'] if body[method.routing_key]['lo']['tx']['lo'] < metrics[method.routing_key]['lo']['tx']['lo'] else metrics[method.routing_key]['lo']['tx']['lo']
+    metrics[method.routing_key]['eth0']['rx']['hi'] = body[method.routing_key]['eth0']['rx']['hi'] if body[method.routing_key]['eth0']['rx']['hi'] > metrics[method.routing_key]['eth0']['rx']['hi'] else metrics[method.routing_key]['eth0']['rx']['hi']
+    metrics[method.routing_key]['eth0']['rx']['lo'] = body[method.routing_key]['eth0']['rx']['lo'] if body[method.routing_key]['eth0']['rx']['lo'] < metrics[method.routing_key]['eth0']['rx']['lo'] else metrics[method.routing_key]['eth0']['rx']['lo']
+    metrics[method.routing_key]['eth0']['tx']['hi'] = body[method.routing_key]['eth0']['tx']['hi'] if body[method.routing_key]['eth0']['tx']['hi'] > metrics[method.routing_key]['eth0']['tx']['hi'] else metrics[method.routing_key]['eth0']['tx']['hi'] 
+    metrics[method.routing_key]['eth0']['tx']['lo'] = body[method.routing_key]['eth0']['tx']['lo'] if body[method.routing_key]['eth0']['tx']['lo'] < metrics[method.routing_key]['eth0']['tx']['lo'] else metrics[method.routing_key]['eth0']['tx']['lo']
+    metrics[method.routing_key]['wlan0']['rx']['hi'] = body[method.routing_key]['wlan0']['rx']['hi'] if body[method.routing_key]['wlan0']['rx']['hi'] > metrics[method.routing_key]['wlan0']['rx']['hi'] else metrics[method.routing_key]['wlan0']['rx']['hi']
+    metrics[method.routing_key]['wlan0']['rx']['lo'] = body[method.routing_key]['wlan0']['rx']['lo'] if body[method.routing_key]['wlan0']['rx']['lo'] < metrics[method.routing_key]['wlan0']['rx']['lo'] else metrics[method.routing_key]['wlan0']['rx']['lo']
+    metrics[method.routing_key]['wlan0']['tx']['hi'] = body[method.routing_key]['wlan0']['tx']['hi'] if body[method.routing_key]['wlan0']['tx']['hi'] > metrics[method.routing_key]['wlan0']['tx']['hi'] else metrics[method.routing_key]['wlan0']['tx']['hi']
+    metrics[method.routing_key]['wlan0']['tx']['lo'] = body[method.routing_key]['wlan0']['tx']['lo'] if body[method.routing_key]['wlan0']['tx']['lo'] < metrics[method.routing_key]['wlan0']['tx']['lo'] else metrics[method.routing_key]['wlan0']['tx']['lo']
     if (method.routing_key == "host1"):
         posts1.insert_one(json.loads(body.decode("utf-8")))
     elif (method.routing_key == "host2"):
         posts1.insert_one(json.loads(body.decode("utf-8")))
+    print(metrics)
 #    print(" [x] Rxd from: " + method.routing_key + " Message: " + str(body))
 
 # establish a connection with the rabbitmq broker
@@ -133,4 +140,4 @@ connection = rmq.rmq_connection(address, vhost, usr, pswd)
 print(" [x] Listening for messages...")
 
 # start listening for messages with the routing keys listed in the second argument
-rmq.rmq_subscribe(connection, ["host1", "host2"], callback)
+rmq.rmq_subscribe(connection, routing_key, callback)
